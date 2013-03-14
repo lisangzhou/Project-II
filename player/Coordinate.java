8a3fbc7c6e881d832cfd49bcd2d80956a2a9f6ed
Eric Wu <wu.eric.g@gmail.com>
2013-03-14 00:05:06 -0700

    Added constructor to Coordinate.java
    Added a no parameter constructor to Coordinate.java to represent the
    Coordinate for a point that does not exist

--- player/Coordinate.java
+++ player/Coordinate.java
@@ -3,7 +3,7 @@ public class Coordinate{
   private int y;
   
   /**
-  * Coordinate() is the two-parameter constructor for a Coordinate object
+  * Coordinate(int x, int y) is the two-parameter constructor for a Coordinate object
   * @param x is the x coordinate of the Coordinate object
   * @param y is the y coordinate of the Coordinate object
   **/
@@ -12,6 +12,15 @@ public class Coordinate{
     this.y = y;
   }
 
+  /**
+  * Coordinate() is the no parameter constructor for a Coordinate object
+  *   It creates a new Coordinate object and sets both x and y to POSITIVE_INFINITY to represent
+  *   a point that does not exist
+  **/
+  public Coordinate(){
+    x = Double.POSITIVE_INFINITY;
+    y = DOUBLE.POSITIVE_INFINITY;
+  }
   /**
   * getX() is a getter for the x instance variable. It returns the x coordinate of a Coordinate object
   **/
