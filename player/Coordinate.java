42b2e8041d42c2a159661340b96f084dc8b574c2
lisangzhou <lzhou@berkeley.edu>
2013-04-04 20:32:09 -0700

    comments coordinate.java

--- player/Coordinate.java
+++ player/Coordinate.java
@@ -5,8 +5,8 @@ public class Coordinate{
   
   /**
   * Coordinate(int x, int y) is the two-parameter constructor for a Coordinate object
-  * @param a is the x coordinate of the Coordinate object
-  * @param b is the y coordinate of the Coordinate object
+  * @param a: the x coordinate of the Coordinate object
+  * @param b: the y coordinate of the Coordinate object
   **/
   public Coordinate(int a, int b){
     x = a;
@@ -14,32 +14,34 @@ public class Coordinate{
   }
 
   /**
-  * Coordinate() is the no parameter constructor for a Coordinate object
-  *   It creates a new Coordinate object and sets both x and y to Integer.MAX_VALUE to represent
-  *   a point that does not exist
+  *  Coordinate() is the no parameter constructor for a Coordinate object
+  *  Creates a new Coordinate object and sets both x and y to Integer.MAX_VALUE to represent
+  *  a point that does not exist
   **/
   public Coordinate(){
     x = Integer.MAX_VALUE;
     y = Integer.MAX_VALUE;
   }
   /**
-  * getX() is a getter for the x instance variable. It returns the x coordinate of a Coordinate object
+  *  getX() is a getter for the x instance variable. 
+  *  return the x coordinate of a Coordinate object
   **/
   public int getX(){
     return this.x;
   }
 
   /**
-  * getY() is a getter for the y instance variable. It returns the y coordinate of a Coordinate object
+  *  getY() is a getter for the y instance variable. 
+  *  return the y coordinate of a Coordinate object
   **/
   public int getY(){
     return this.y;
   }
 
   /**
-  * equals() checks if two Coordinate objects are equal. This method returns true if both Coordinates
-  *   have the same x values and the same y values
-  * @param other is the Coordinate being compared to
+  *  equals() checks if two Coordinate objects are equal. This method returns true if both Coordinates
+  *  have the same x values and the same y values
+  *  @param other: the Coordinate being compared to
   **/
   public boolean equals(Coordinate other){
     return x == other.getX() && y == other.getY();
